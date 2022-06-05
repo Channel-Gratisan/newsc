c42379d6aa1fd6534119addb9b6b994fbd2a5769
MasWay <70774862+Channel-Gratisan@users.noreply.github.com>
2022-06-06 04:13:25 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-cyan='\x1b[96m'
-white='\x1b[37m'
-bold='\033[1m'
-off='\x1b[m'
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
 
-read -rp "Masukkan Domain Anda : " -e host
-sleep 1
-  echo -e "${green}Pastikan Domain Anda Sudah DiPointing Sebelumnya!${off}"
-sleep 1
-ptrg="$(cat ~/log-install.txt | grep -i TrojanGO | cut -d: -f2|sed 's/ //g')"
-uuid=$(cat /etc/trojan-go/uuid.txt)
-echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" >>/var/lib/crot/ipvps.conf
+rm -rf /etc/xray/domain
+echo $domain > /etc/vray/domain
+certv2ray
 cat <<EOF > /etc/trojan-go/config.json
 {
     "run_type": "server",
